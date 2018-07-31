eb03b1b99af5738b10d89701be292033c9e27811
RuiHKUST <rzhouae@connect.ust.hk>
2018-07-31 19:45:35 +0800

    Implement efficient algorithm for ML estimation

--- R/covFactorModel.R
+++ R/covFactorModel.R
@@ -12,7 +12,8 @@
 #'        \itemize{\item \code{"M"} - macroeconomic factor model, requires user to pass \code{econ_fact}
 #'                 \item \code{"B"} - BARRA Industry factor model, requires user to pass \code{stock_sector_info} 
 #'                  or colnames of \code{X} to be contained in the in-built database \code{data(stock_sector_database)}
-#'                 \item \code{"S"} - statistical factor model, requires user to pass number of factors \code{K} (default)} 
+#'                 \item \code{"S"} - statistical factor model, requires user to pass number of factors \code{K} (default)
+#'                 \item \code{"ML"} - Maximum likelihood estimation under factor model structure }
 #' @param econ_fact xts object of dimension \eqn{T x K}, required and used when \code{type = "M"}
 #' @param K number of factors when build a statistical factor model, used when \code{type = "S"} (default: \eqn{1})
 #' @param orthonormal string object indicating position of normalization in the statistical factor 
@@ -64,9 +65,83 @@
 covFactorModel <- function(X, type = "S", econ_fact = NA,
                            K = 1, orthonormal = "factor", max_iter = 1, tol = 1e-3, 
                            Psi_struct = "diag", stock_sector_info = NA) {
-  Sigma <- factorModel(X, type, econ_fact,
-                       K, orthonormal, max_iter, tol, 
-                       Psi_struct, stock_sector_info, TRUE)$Sigma
+  if (type == "ML") {
+    Sigma <- cov_factor_ML(cov(X), K, 0)
+  } else {
+    Sigma <- factorModel(X, type, econ_fact,
+                         K, orthonormal, max_iter, tol, 
+                         Psi_struct, stock_sector_info, TRUE)$Sigma
+  }
   
   return(Sigma)
+}
+
+# implement the efficient algorithm 
+# to calculate maximum likelihood estimator in low rank covariance matrix
+# minimize   log det(Sigma) + Tr(inv(Sigma) * S)
+# subject to Sigma = BB' + Psi
+#            Psi = diag(psi1, ..., psip) >= 0
+
+# implement of efficient algorithm
+cov_factor_ML <- function(S, K, epsilon, tol = 1e-3, max_iter = 100) {
+  
+  # ad-hoc initialization by trivial estimation
+  tmp <- eigen(x = S, symmetric = TRUE)
+  U <- cbind(tmp$vectors[, 1:K])
+  D <- tmp$values[1:K]
+  psi_vec <- diag(S - U %*% diag(D, K) %*% t(U))
+  phi_vec <- 1 / psi_vec
+  
+  # iterately solve problem by phi (inverse of psi)
+  for (loop in 1:max_iter) {
+    phi_vec_old <- phi_vec
+    
+    subgradient <- sub_grad(psi_vec, S, K)
+    phi_vec <- pmin(1 / (diag(S) - subgradient), 1 / epsilon)
+    psi_vec <- 1 / phi_vec
+    
+    diff <- norm(phi_vec - phi_vec_old, "2") / norm(phi_vec_old, "2")
+    if (diff < tol) break
+  }
+  
+  # recover B and the Sigma
+  B <- recover_loading_matrix(S, K, psi_vec)
+  Sigma <- B %*% t(B) + diag(psi_vec)
+  
+  return(Sigma)
+}
+
+
+
+# subgradient of the sub-problem (see reference)
+sub_grad <- function(psi_vec, S, K) {
+  phi_inv_sqrt <- sqrt(psi_vec)
+  phi_sqrt <- 1 / phi_inv_sqrt
+  
+  tmp <- eigen(x = S*(phi_sqrt %*% t(phi_sqrt)), symmetric = TRUE)
+  U <- cbind(tmp$vectors[, 1:K])
+  D <- tmp$values[1:K]
+  D1 <- pmax(0, 1 - 1/D)
+  
+  subgradient <- diag( ((U%*%diag(D1, K)%*%t(U)) * (phi_inv_sqrt%*%t(phi_sqrt))) %*% S)
+  
+  return(subgradient)
+}
+
+# recover B by psi_vec
+recover_loading_matrix <- function(S, K, psi_vec) {
+  psi_sqrt <- sqrt(psi_vec)
+  psi_inv_sqrt <- 1 / psi_sqrt
+  tmp <- eigen(S * (psi_inv_sqrt%*%t(psi_inv_sqrt)))
+  U <- cbind(tmp$vectors[, 1:K])
+  D <- tmp$values[1:K]
+  Z <- matrix(0, nrow(S), K)
+  
+  for (i in 1:K) {
+    zi <- U[, i]
+    Z[, i] <- zi * sqrt(max(1, D[i]) - 1) / norm(zi, "2")
+  }
+  
+  B <- diag(psi_sqrt) %*% Z;
+  return(B)
 }
